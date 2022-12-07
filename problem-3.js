d6b8b50ba620bd3c351b068bd179818f9a8247a6
Rishabh157 <rishabhgour157@gmail.com>
2022-12-07 23:50:54 +0530

    problem-3 file added get the first elements of and given parameter array

---
+++ problem-3.js
@@ -0,0 +1,25 @@
+// Write a JavaScript function to get the first element of an array. Passing a parameter 'n' will return the first 'n' elements of the array
+
+// 1. logic
+
+const getElementsOfParameter = function (array, num) {
+
+    let newArr = []
+
+    if (array.length === 0 && num < 0 || num > array.length) {
+        return array
+    } else if (num === undefined) {
+        return array[0]
+    } else {
+        for (let i = 0; i < num; i++) {
+            newArr.push(array[i])
+        }
+    }
+    return newArr
+}
+
+
+let array = ["India", "Argentina", "Portgal", "France", "USA", "England"]
+
+let result = getElementOfNumArray(array, 4)
+console.log(result)
