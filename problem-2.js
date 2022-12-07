b3aca938396889086520a80caa0d3017536329cd
Rishabh157 <rishabhgour157@gmail.com>
2022-12-07 23:12:56 +0530

    problem-2 file added clone array

---
+++ problem-2.js
@@ -0,0 +1,30 @@
+// Write a JavaScript function to clone an array
+
+// 1. logic
+
+const copyOfArrayWithJsMethods = function (array) {
+    let cloneArray = array.slice(0)
+    return cloneArray
+}
+
+let arr = ["india", "America", "Caneda", "New-York", "Norway"]
+
+let array = copyOfArrayWithJsMethods(arr)
+console.log(array)
+
+
+// 2. logic
+
+const cloneArrayWithLogic = function (array) {
+
+    let newArray = []
+    for (let i = 0; i < array.length; i++) {
+        newArray[i] = array[i]
+    }
+    return newArray
+}
+
+let arrayOfCountries = ["india", "America", "Caneda", "New-York", "Norway"]
+
+let cloneArray = cloneArrayWithLogic(arrayOfCountries)
+console.log(cloneArray)
